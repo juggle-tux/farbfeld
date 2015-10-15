bc0f1f9ef47397c368e8bd841adbd5d3e4933f7c
Peter Hellberg <peter@c7.se>
2015-10-15 23:11:25 +0200

    Replaced Imagefile struct with image.RGBA

--- decode.go
+++ decode.go
@@ -12,54 +12,46 @@ import (
 	"io/ioutil"
 )
 
-type Imagefile struct {
-	Width  uint32
-	Height uint32
-	Buf    [][]color.RGBA // Buf[y][x]
-}
-
-func (i Imagefile) ColorModel() color.Model {
-	return color.RGBAModel
-}
-func (i Imagefile) Bounds() image.Rectangle {
-	return image.Rect(0, 0, int(i.Width), int(i.Height))
-}
-func (i Imagefile) At(x, y int) color.Color {
-	if !(image.Point{x, y}.In(i.Bounds())) {
-		return color.RGBA{}
-	}
-	return i.Buf[y][x]
-}
-
+// Decode reads a Imagefile image from r and returns it as an image.Image.
 func Decode(r io.Reader) (image.Image, error) {
-	var img Imagefile
 	bb := bufio.NewReader(r)
 
-	io.CopyN(ioutil.Discard, bb, 9)
-
-	if err := binary.Read(bb, binary.BigEndian, &img.Width); err != nil {
-		return nil, err
-	}
-	if err := binary.Read(bb, binary.BigEndian, &img.Height); err != nil {
+	if _, err := io.CopyN(ioutil.Discard, bb, 9); err != nil {
 		return nil, err
 	}
 
-	img.Buf = make([][]color.RGBA, img.Height)
-	for y := range img.Buf {
-		img.Buf[y] = make([]color.RGBA, img.Width)
-		for x := range img.Buf[y] {
+	var (
+		width  uint32
+		height uint32
+	)
+
+	if err := binary.Read(bb, binary.BigEndian, &width); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(bb, binary.BigEndian, &height); err != nil {
+		return nil, err
+	}
+
+	w := int(width)
+	h := int(height)
+
+	img := image.NewRGBA(image.Rect(0, 0, w, h))
+
+	for y := 0; y < h; y++ {
+		for x := 0; x < w; x++ {
 			rgba := make([]byte, 4)
+
 			if _, err := io.ReadFull(bb, rgba); err == io.EOF {
 				return nil, io.ErrUnexpectedEOF
 			} else if err != nil {
 				return nil, err
 			}
-			img.Buf[y][x] = color.RGBA{rgba[0], rgba[1], rgba[2], rgba[3]}
+
+			img.Set(x, y, color.RGBA{rgba[0], rgba[1], rgba[2], rgba[3]})
 		}
 	}
 
 	return img, nil
-
 }
 
 // DecodeConfig returns an empty image.Config:
--- decode_test.go
+++ decode_test.go
@@ -12,12 +12,6 @@ var (
 	g = color.RGBA{0, 255, 0, 255}
 	b = color.RGBA{0, 0, 255, 255}
 
-	testBuf = [][]color.RGBA{
-		{r, g, b},
-		{b, r, g},
-		{g, b, r},
-	}
-
 	imagefileTestCases = []struct {
 		x int
 		y int
@@ -39,46 +33,6 @@ var (
 		"\xff\x00\x00\xff\xff\x00\x00\xff\xff\xff\x00\x00\xff\x00\xff" +
 		"\x00\xff\x00\xff\x00\xff\x00\x00\xff\xff\xff\x00\x00\xff")
 )
-
-func TestColorModel(t *testing.T) {
-	m := Imagefile{}
-
-	if got, want := m.ColorModel(), color.RGBAModel; got != want {
-		t.Fatalf(`m.ColorModel() = %v, want %v`, got, want)
-	}
-}
-
-func TestBounds(t *testing.T) {
-	for i, tt := range []struct {
-		w uint32
-		h uint32
-	}{
-		{100, 100},
-		{200, 100},
-		{100, 200},
-	} {
-		m := Imagefile{Width: tt.w, Height: tt.h}
-
-		if b := image.Rect(0, 0, int(tt.w), int(tt.h)); !m.Bounds().Eq(b) {
-			t.Fatalf(`[%d] m.Bounds() = %v, want %v`, i, m.Bounds(), b.Bounds())
-		}
-	}
-}
-
-func TestAt(t *testing.T) {
-	m := Imagefile{Buf: testBuf}
-
-	for i, tt := range imagefileTestCases {
-		got := m.At(tt.x, tt.y)
-
-		gr, gg, gb, ga := got.RGBA()
-		wr, wg, wb, wa := tt.c.RGBA()
-
-		if gr != wr || gg != wg || gb != wb || ga != wa {
-			t.Fatalf(`[%d] m.At(tt.x, tt.y).RGBA() = %v, want %v`, i, got, tt.c)
-		}
-	}
-}
 
 func TestDecode(t *testing.T) {
 	m, err := Decode(bytes.NewReader(imagefileData))
